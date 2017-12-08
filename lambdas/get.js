cdfd72fe809e70553984e625e4a353e8893570c7
ageronimo <angelageroniimo@gmail.com>
2017-12-08 10:04:51 -1000

    edited post in yaml, added stuff to get

--- lambdas/get.js
+++ lambdas/get.js
@@ -1,3 +1,25 @@
+'use strict';
+
+// require in pg pool
+const Pool =
+const config = require('./config.json');
+const { table, host, database, user, passord, port } = config
+// object destructuring
+const Client = new Pool({
+  host,
+  database,
+  user,
+  password,
+  port,
+  idleTimeoutMillis: 1000
+});
+// how do i connect to my database?
+
+// make sql query string that is used to get data from db
+// CORS
+
 module.exports.get = (param) => {
+
+  // .connect(), .then(), .release(), .query()
 
 };
