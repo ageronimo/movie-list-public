241e405e508c31fceb091c2726fbc019f5024239
ageronimo <angelageroniimo@gmail.com>
2018-01-08 09:33:49 -1000

    started update lamb, need update query

--- lambdas/update.js
+++ lambdas/update.js
@@ -1,3 +1,43 @@
-module.exports.update = (param) => {
+const Pool = require('pg-pool');
+const config = require('./config.json');
+const { table, host, database, user, password, port } = config;
+const Client = new Pool({
+  host,
+  database,
+  user,
+  password,
+  port,
+  idleTimeoutMillis: 1000
+});
+const data = require('../test-data/update.json');
+
+// let values = ['id', 'title', 'year', 'genre'];
+console.log(data);
+
+module.exports.post = (event, context, callback) => {
+    let updateMovie = //update query
+    let getAllMovies = 'select * from movies;';
+
+    Client.connect()
+    .then(client => {
+      console.log('connected to DB' + Client.options.database);
+      client.release();
+      client.query(update);
+      return client.query(getAllMovies)
+    })
+    .then(res => {
+      const response = {
+        statusCode: 200,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Credentials': true
+        },
+        body: JSON.stringify({
+          message: res
+        }),
+      };
+      callback(null, response);
+
+      })
 
 };
